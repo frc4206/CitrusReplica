e530c6b73f2f2bc4f8d3e1a2a7566471dfaf174d
JPSeidlitz <seidfam3@gmail.com>
2024-11-15 19:28:50 -0600

    added pivot

---
+++ src/main/java/frc/robot/subsystems/Pivot_Sub.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.common.DefaultTalonFX;
+import frc.robot.common.LoadableConfig;
+
+public class Pivot_Sub extends SubsystemBase {
+  /** Creates a new Pivot_Sub. */
+  DefaultTalonFX.Config motorConfig = new DefaultTalonFX.Config("pivotMotor");
+  
+  public class Config extends LoadableConfig {
+
+    public Config(String filename) {
+      int cow;
+
+			super.load(this, filename);
+			LoadableConfig.print(this);
+		}
+  }
+
+  public DefaultTalonFX m_pivotMotor = new DefaultTalonFX(45, motorConfig);
+  public Pivot_Sub() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
