8a8c2d1d8a2db4d3b2f709375faa0e774bca8aa6
KelliB2 <77307653+KelliB2@users.noreply.github.com>
2024-11-15 20:21:54 -0600

    actually commiting this time

--- src/main/java/frc/robot/subsystems/Amp_Sub.java
+++ src/main/java/frc/robot/subsystems/Amp_Sub.java
@@ -6,15 +6,40 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.common.DefaultTalonFX;
+import frc.robot.common.LoadableConfig;
 
 public class Amp_Sub extends SubsystemBase {
   /** Creates a new Amp_Sub. */
-  public DefaultTalonFX m_ampMotor = new DefaultTalonFX(50);
 
-  public Amp_Sub() {}
+  DefaultTalonFX.Config ampConfig = new DefaultTalonFX.Config("ampMotor");
+
+  public class Config extends LoadableConfig {
+  
+    public Config(String filename) {
+      int crab;
+
+      super.load(this, filename);
+      LoadableConfig.print(this);
+    }
+    
+  }
+  public DefaultTalonFX m_ampMotor = new DefaultTalonFX(50, ampConfig);
+
+  public Amp_Sub() {
+    m_ampMotor.Enable_Sim();
+    //m_ampMotor.Log_Data_To_Smart_Dashboard();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    m_ampMotor.Update();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    super.simulationPeriodic();
+    m_ampMotor.Update_Sim();
+    m_ampMotor.Duty_Cycle(0.5, false);
   }
 }
--- src/main/java/frc/robot/subsystems/Elevator_Sub.java
+++ src/main/java/frc/robot/subsystems/Elevator_Sub.java
@@ -6,15 +6,45 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.common.DefaultTalonFX;
+import frc.robot.common.LoadableConfig;
 
 public class Elevator_Sub extends SubsystemBase {
   /** Creates a new Elevator_Sub. */
-  public DefaultTalonFX m_elevatorMotor = new DefaultTalonFX(60);
+
+  DefaultTalonFX.Config elevatorConfig = new DefaultTalonFX.Config("elevatorMotor");
+
+  public class Config extends LoadableConfig {
+
+    public Config(String filename) {
+      int fliegerfaust;
+
+      super.load(this, filename);
+      LoadableConfig.print(this);
+    }
+  }
+  public DefaultTalonFX m_elevatorMotor1 = new DefaultTalonFX(60, elevatorConfig);
+  public DefaultTalonFX m_elevatorMotor2 = new DefaultTalonFX(61,elevatorConfig);
   
-  public Elevator_Sub() {}
+  public Elevator_Sub() {
+    m_elevatorMotor1.Enable_Sim();
+    m_elevatorMotor2.Enable_Sim();
+    //m_elevatorMotor1.Log_Data_To_Smart_Dashboard();
+    //m_elevatorMotor2.Log_Data_To_Smart_Dashboard();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    m_elevatorMotor1.Update();
+    m_elevatorMotor2.Update();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    super.simulationPeriodic();
+    m_elevatorMotor1.Update_Sim();
+    m_elevatorMotor2.Update_Sim();
+    m_elevatorMotor1.Duty_Cycle(0.5, false);
+    m_elevatorMotor2.Duty_Cycle(0.5, false);
   }
 }
